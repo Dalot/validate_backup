2f9d76c8b13605f9b0f77fbee9af3a5499d44037
Rogério Dalot <ge.dalot@gmail.com>
2021-02-06 20:17:56 +0000

    the best we can do without channels and a db

--- stream/stream.go
+++ stream/stream.go
@@ -24,68 +24,31 @@ type Data struct {
 }
 
 type Obj struct {
-	Data  Data
-	bytes int64
-	start int64 //byte offset start
-	end   int64 // /byte offset end
+	Id   string
+	Name string
 }
 
-type List []Obj
+type List map [string]Obj
 
 type Stream struct{}
-
-// beforeBackupIDMap is a map that will point the id to an index for the BeforeBackup List
-var beforeBackupIDMap = map[string]uint64{}
-
-// afterBackupIDMap is a map that will point the id to an index for the AfterBackup List
-var afterBackupIDMap = map[string]uint64{}
 
 // Compare takes two readers, each to a json file, and compares them to find if they are equal
 func Compare(beforeReader io.Reader, afterReader io.Reader) (*Stream, Comparison) {
 	stream := &Stream{}
 
-	listBeforeBackup, comp := stream.Parse(beforeReader, beforeBackupIDMap)
+	listBeforeBackup, comp := stream.ParseBefore(beforeReader)
 	if comp != None {
 		return stream, comp
 	}
-	listAfterBackup, comp := stream.Parse(afterReader, afterBackupIDMap)
-	if comp != None {
-		return stream, comp
-	}
+	comp = stream.Compare(afterReader, listBeforeBackup)
 
-	result := stream.compare(listBeforeBackup, listAfterBackup)
-
-	return stream, result
-}
-
-func (s *Stream) compare(listBeforeBackup *List, listAfterBackup *List) Comparison {
-
-	equalFoundObjs := 0
-	for _, obj := range *listBeforeBackup {
-		if index, exists := afterBackupIDMap[obj.Data.Id]; !exists {
-			log.Println("Did not find id(", obj.Data.Id, ") in the file after the backup")
-			return Different
-		} else {
-
-			if obj.Data.Name == (*listAfterBackup)[index].Data.Name {
-				log.Print("Found id(", obj.Data.Id, ") in both files with the same data")
-				log.Print("Location: beforeBackup index:", beforeBackupIDMap[obj.Data.Id], ", afterBackup:", index)
-				equalFoundObjs++
-			} else {
-				return Different
-			}
-		}
-	}
-
-	log.Println("equalFoundObjs: (", equalFoundObjs, ")")
-
-	return Equal
+	return stream, comp
 }
 
 // Parse does stream processing of json file => less memory footprint, as we read record by record
-func (s *Stream) Parse(input io.Reader, idMap map[string]uint64) (*List, Comparison)  {
+func (s *Stream) ParseBefore(input io.Reader) (map [string]Obj, Comparison)  {
 	var count uint64 = 0
-	var list *List = &List{}
+	var list = make(map [string]Obj)
 	decoder := json.NewDecoder(input)
 
 	// read open bracket
@@ -97,30 +60,20 @@ func (s *Stream) Parse(input io.Reader, idMap map[string]uint64) (*List, Compari
 
 	// while the array contains values
 	for decoder.More() {
-		var data Data
+		var obj Obj
 		// decode an array value (Message)
-		currentOffset := decoder.InputOffset()
-		err := decoder.Decode(&data)
+		err := decoder.Decode(&obj)
 
 		if err != nil {
 			log.Fatal("[2]", err)
 		}
-
-		finalOffset := decoder.InputOffset()
-		obj := Obj{
-			Data:  data,
-			start: currentOffset,
-			end:   finalOffset,
-			bytes: finalOffset - currentOffset,
+		
+		newObj := Obj{
+			Name: obj.Name,
+			Id: obj.Id,
 		}
+		list[obj.Id] = newObj
 
-		if index, exists := idMap[data.Id]; exists {
-			log.Println("IDs should be unique. The id:", data.Id, " is already present and has value index of:", index)
-			return &List{}, DuplicateIds
-		}
-
-		idMap[data.Id] = count
-		*list = append(*list, obj)
 		count++
 	}
 
@@ -137,9 +90,56 @@ func (s *Stream) Parse(input io.Reader, idMap map[string]uint64) (*List, Compari
 	return list, None
 }
 
-func (s *Stream) Flush() {
-	beforeBackupIDMap = map[string]uint64{}
-	afterBackupIDMap = map[string]uint64{}
+
+// Parse does stream processing of json file => less memory footprint, as we read record by record
+func (s *Stream) Compare(input io.Reader, listBeforeBackup map[string]Obj) (Comparison)  {
+	var count uint64 = 0
+	decoder := json.NewDecoder(input)
+
+	// read open bracket
+	t, err := decoder.Token()
+	if err != nil {
+		log.Fatal("[1]", err)
+	}
+	fmt.Printf("%T: %v\n", t, t)
+
+	// while the array contains values
+	for decoder.More() {
+		var obj Obj
+		// decode an array value (Message)
+		err := decoder.Decode(&obj)
+
+		if err != nil {
+			log.Fatal("[2]", err)
+		}
+
+		if existentObj, exists := listBeforeBackup[obj.Id]; !exists {
+			log.Println("IDs should be unique. The id:", existentObj.Id, " is already present")
+			return DuplicateIds
+		} else {
+			if existentObj.Name != obj.Name {
+				log.Print("Files are different")
+				log.Print("Before File: id(", existentObj.Id, "), value(", existentObj.Name,")")
+				log.Print("After file: id(", obj.Id, "), value(", obj.Name,")")
+				return Different
+
+			} else {
+				delete(listBeforeBackup, obj.Id)
+			}
+		}
+	}
+
+	// read closing bracket
+	t, err = decoder.Token()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%T: %v\n", t, t)
+	fmt.Println("--------")
+	fmt.Println("count : = " + strconv.FormatUint(count, 10))
+	fmt.Println("--------")
+
+	return Equal
 }
 
 func (c Comparison) String() string {
--- stream/stream_test.go
+++ stream/stream_test.go
@@ -10,26 +10,59 @@ var cases = []struct {
 	b      string
 	result Comparison
 }{
-	{`[{"id": "5","name": "John doe"}]`,`[{"id": "5","name": "John doe"}]`, Equal},
-	{`[{"id": "5","name": "John doe"}]`,`[{"id": "5","name": "John doe"}]`, Equal},
-	{`[{"id": "5","name": "John doe"}]`,`[{"id": "5","name": "42"}]`, Different},
-	{`[{"id": "5","name": "John doe"},{"id": "6","name": "John doe"}]`,`[{"id": "6","name": "John doe"},{"id": "5","name": "John doe"}]`, Equal}, 
-	{`[{"id": "4","name": "John doe"}]`,`[{"id": "5","name": "John doe"}]`, Different},
-	{`[{"id": "5","name": "John doe"},{"id": "5","name": "John doe"}]`,`[{"id": "6","name": "John doe"},{"id": "5","name": "John doe"}]`, DuplicateIds}, 
-	{`[{"id": "5","name": "John doe"},{"id": "6","name": "John doe"}]`,`[{"id": "6","name": "John doe"},{"id": "6","name": "John doe"}]`, DuplicateIds}, 
+	{`[{"id": "5","name": "John doe"}]`, `[{"id": "5","name": "John doe"}]`, Equal},
+	{`[{"id": "5","name": "John doe"}]`, `[{"id": "5","name": "John doe"}]`, Equal},
+	{`[{"id": "5","name": "John doe"}]`, `[{"id": "5","name": "42"}]`, Different},
+	{`[{"id": "5","name": "John doe"},{"id": "6","name": "John doe"}]`, `[{"id": "6","name": "John doe"},{"id": "5","name": "John doe"}]`, Equal},
+	{`[{"id": "4","name": "John doe"}]`, `[{"id": "5","name": "John doe"}]`, Different},
+	{`[{"id": "5","name": "John doe"},{"id": "5","name": "John doe"}]`, `[{"id": "6","name": "John doe"},{"id": "5","name": "John doe"}]`, DuplicateIds},
+	{`[{"id": "5","name": "John doe"},{"id": "6","name": "John doe"}]`, `[{"id": "6","name": "John doe"},{"id": "6","name": "John doe"}]`, DuplicateIds},
 }
 
 func TestCompare(t *testing.T) {
 	for i, c := range cases {
 		a := bytes.NewReader([]byte(c.a))
 		b := bytes.NewReader([]byte(c.b))
-		
-		stream, result := Compare(a, b)
-		
+
+		_, result := Compare(a, b)
+
 		if result != c.result {
 			t.Errorf("case %d failed, got: %v, expected: %v", i, result, c.result)
 		}
 
-		stream.Flush()
 	}
+}
+
+func BenchmarkObjects(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		getObjects(7000000)
+	}
+}
+
+func BenchmarkSlice(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		getSlice(7000000)
+	}
+}
+
+func getObjects(n int) List {
+	list := make(map[string]Obj, n)
+	for i := range list {
+		list[i] = Obj{
+			Name: "John",
+			Id:   "Doe",
+		}
+	}
+	return list
+}
+
+func getSlice(n int) []Obj {
+	list := make([]Obj, n)
+	for i := range list {
+		list[i] = Obj{
+			Name: "John",
+			Id:   "Doe",
+		}
+	}
+	return list
 }
